a6183d0b3fd16e4c8735404eb715706157a2af52
LukasKanne <72263330+LukasKanne@users.noreply.github.com>
2020-11-14 19:26:39 +0100

    added config properties

--- src/config.py
+++ src/config.py
@@ -1,1 +1,12 @@
-INTERVAL_SIZE = 101
+INTERVAL_SIZE = 1000
+
+# plot properties
+
+
+MARKER_STYLE_DATA = 'o'
+COLOR_DATA = 'red'
+
+COLOR_SPLINE = 'black'
+
+LEGEND = False
+COORDINATE_SYSTEM = False
