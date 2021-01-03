3481ca04883418d73205b8418b3634af5cade9f7
LukasKanne <72263330+LukasKanne@users.noreply.github.com>
2021-01-03 13:56:11 +0100

    moved files

--- index.py
+++ index.py
@@ -3,11 +3,11 @@ import src.data_creation as data_creation
 import src.curve_creation as curve_creation
 
 
-def start():
-    data_creation.setup_coordinate_system()
+def create_curve():
+    plot.setup_coordinate_system()
     points_on_curve = data_creation.get_input_points()
     x_line, y_line = curve_creation.create_curve(points_on_curve)
     plot.plot_data(points_on_curve, x_line, y_line)
 
 
-start()
+create_curve()
