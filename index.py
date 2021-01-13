6d4d79aedfe714f4c5104f1c07384607e348a097
LukasKanne <72263330+LukasKanne@users.noreply.github.com>
2021-01-14 00:24:44 +0100

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import src.curve_creation as curve_creation
 def create_curve():
     plot.setup_coordinate_system()
     points_on_curve = data_creation.get_input_points()
-    x_line, y_line = curve_creation.create_curve(points_on_curve)
+    x_line, y_line = curve_creation.create_spline(points_on_curve)
     plot.plot_data(points_on_curve, x_line, y_line)
 
 
