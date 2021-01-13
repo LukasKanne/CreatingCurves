5d717197be29ff62dec11c310af8f92a983de49f
LukasKanne <72263330+LukasKanne@users.noreply.github.com>
2021-01-14 00:25:00 +0100

    refactor

--- src/data_creation.py
+++ src/data_creation.py
@@ -5,14 +5,13 @@ import src.plot as plot
 
 
 def get_input_points():
-    plt.waitforbuttonpress()
+
     listener = keyboard_input.start_listener()
     data_points = []
     while listener.running:
         plot.instruction("if you are ready, type escape key")
         data_points = np.asarray(plt.ginput(1, timeout=-1))
         plt.plot(data_points[:, 0], data_points[:, 1], marker='o', color='blue', linestyle='')
-
         while listener.running:
             plot.instruction("select points of curve in order they are met")
 
